90ea21c477b06840e499949d9711a6b95163f465
AxinYyds1231 <3887368442@qq.com>
2026-07-05 16:46:21 +0800

    login1

--- functions/api/login.js
+++ functions/api/login.js
@@ -1,13 +1,17 @@
 // functions/api/login.js
+const CORS_HEADERS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Content-Type': 'application/json'
+};
+
 export async function onRequest(context) {
     return new Response(JSON.stringify({
         success: true,
         message: 'login.js 工作正常！'
     }), {
         status: 200,
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        }
+        headers: CORS_HEADERS
     });
 }
